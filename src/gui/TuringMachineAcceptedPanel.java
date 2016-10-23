3a574effdef694baa70a48970bd851c19d02f7e0
Adrián <arodriba@ull.edu.es>
2016-10-23 01:31:49 +0100

    Improving TM Accepted Panel (orange color for unknown)

--- src/gui/TuringMachineAcceptedPanel.java
+++ src/gui/TuringMachineAcceptedPanel.java
@@ -15,30 +15,32 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class TuringMachineAcceptedPanel extends JPanel {
-	private Boolean accepted;
-	
-	public TuringMachineAcceptedPanel() {		
-	}
-	
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g.create());
-		
-		if (accepted != null) {
-			if (accepted) {
-				g.setColor(Color.GREEN);
-			} else {
-				g.setColor(Color.RED);
-			}
-			
-			g.fillRect(0, 0, getWidth(), getHeight());
-		}
-	}
-	
-	public Boolean getAccepted() {
-		return accepted;
-	}
-	
-	public void setAccepted(Boolean accepted) {
-		this.accepted = accepted;
-	}	
+  public Boolean accepted;
+
+  public TuringMachineAcceptedPanel() {
+    super();
+  }
+
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g.create());
+    if (getAccepted() != null) {
+      if (getAccepted()) {
+        g.setColor(Color.GREEN);
+      } else {
+        g.setColor(Color.RED);
+      }
+      g.fillRect(0, 0, getWidth(), getHeight());
+    } else {
+      g.setColor(Color.ORANGE);
+      g.fillRect(0, 0, getWidth(), getHeight());
+    }
+  }
+
+  public Boolean getAccepted() {
+    return accepted;
+  }
+
+  public void setAccepted(Boolean accepted) {
+    this.accepted = accepted;
+  }	
 }
