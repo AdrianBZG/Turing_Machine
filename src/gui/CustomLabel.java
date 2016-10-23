cd9d23bc6bb049794253ffde36bcbf8fe21ce8e2
Adrián <arodriba@ull.edu.es>
2016-10-23 01:30:51 +0100

    Added FileChooserWindow and CustomLabel classes

---
+++ src/gui/CustomLabel.java
@@ -0,0 +1,35 @@
+/**
+ * @author Adrian Rodriguez Bazaga 
+ * @version 1.0.0
+ * @date 21 October 2016
+ * @email alu0100826456@ull.edu.es / arodriba@ull.edu.es
+ * @subject Complejidad Computacional
+ * @title Multi-Tape Turing Machine
+ */
+
+package gui;
+
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+
+public class CustomLabel extends JLabel {
+  public CustomLabel(String text, boolean bold) {
+    super(text, JLabel.CENTER);
+    if(bold) setText("<html><b>" + getText() + "</b></html>");
+  }
+  
+  public CustomLabel(String text, Color textColor, boolean bold) {
+    super(text, JLabel.CENTER);
+    setForeground(textColor);
+    if(bold) setText("<html><b>" + getText() + "</b></html>");
+  }
+  
+  public CustomLabel(String text, Color textColor, Color borderColor, boolean bold) {
+    super(text, JLabel.CENTER);
+    setForeground(textColor);
+    setBorder(BorderFactory.createLineBorder(borderColor));
+    if(bold) setText("<html><b>" + getText() + "</b></html>");
+  }
+}
---
+++ src/gui/FileChooserWindow.java
@@ -0,0 +1,69 @@
+/**
+ * @author Adrian Rodriguez Bazaga 
+ * @version 1.0.0
+ * @date 21 October 2016
+ * @email alu0100826456@ull.edu.es / arodriba@ull.edu.es
+ * @subject Complejidad Computacional
+ * @title Multi-Tape Turing Machine
+ */
+
+package gui;
+
+import java.awt.BorderLayout;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class FileChooserWindow extends JFrame {
+  private JPanel container = new JPanel();
+  private JFileChooser fileChooser = new JFileChooser();
+
+  public FileChooserWindow() {
+    addElementsToWindow();
+    setWindowSettings();
+  }
+  
+  private void addElementsToWindow() {
+    FileNameExtensionFilter filter = new FileNameExtensionFilter("TM Turing Machine File", "tm");
+    getFileChooser().setFileFilter(filter);
+    getFileChooser().setCurrentDirectory(new File(System.getProperty("user.home")));
+    getContainerElement().add(getFileChooser());
+    add(getContainerElement(), BorderLayout.CENTER);
+  }
+  
+  private void setWindowSettings() {
+    setLayout(new BorderLayout());
+    pack();
+  }
+
+  /**
+   * @return the container
+   */
+  public JPanel getContainerElement() {
+    return container;
+  }
+
+  /**
+   * @param container the container to set
+   */
+  public void setContainer(JPanel container) {
+    this.container = container;
+  }
+
+  /**
+   * @return the fileChooser
+   */
+  public JFileChooser getFileChooser() {
+    return fileChooser;
+  }
+
+  /**
+   * @param fileChooser the fileChooser to set
+   */
+  public void setFileChooser(JFileChooser fileChooser) {
+    this.fileChooser = fileChooser;
+  }
+}
